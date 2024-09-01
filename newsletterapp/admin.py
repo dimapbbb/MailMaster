46fbd3eaecd555cd65fc6691a6b84f6c9e9f52e4
dimapbbb <1st.trillionaire.dm@gmail.com>
2024-09-01 20:11:48 +0300

    create base model newsletter + add model for admin

--- newsletterapp/admin.py
+++ newsletterapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from newsletterapp.models import Newsletter
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('title', 'topic', 'content')
--- newsletterapp/models.py
+++ newsletterapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Newsletter(models.Model):
+
+    title = models.CharField(max_length=50, verbose_name="Заголовок")
+    topic = models.CharField(max_length=200, verbose_name="Тема рассылки")
+    content = models.TextField(verbose_name="Содержимое рассылки")
+
+    def __str__(self):
+        return f"{self.title}, {self.topic}"
+
+    class Meta:
+        verbose_name = "Рассылка"
+        verbose_name_plural = "Рассылки"
+
