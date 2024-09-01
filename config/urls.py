a232cec23bb8b4bf8c1186e7b7314adf5f33a773
dimapbbb <1st.trillionaire.dm@gmail.com>
2024-09-01 23:02:25 +0300

    Create recipients app

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('newsletterapp.urls', namespace='newsletter'))
+    path('', include('newsletterapp.urls', namespace='newsletter')),
+    path('recipients/', include('recepients.urls', namespace='recipients')),
 ]
