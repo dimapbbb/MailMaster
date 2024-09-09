3a9fada512253119a3a25787c33ee85e9b1fcc93
dimapbbb <1st.trillionaire.dm@gmail.com>
2024-09-09 16:19:05 +0300

    Кастомная команда принимает аргумент пк рассылки

--- newsletterapp/management/commands/send_newsletter.py
+++ newsletterapp/management/commands/send_newsletter.py
@@ -7,3 +7,6 @@ class Command(BaseCommand):
 
     def handle(self, newsletter_id, *args, **options):
         send_newsletter(newsletter_id, send_method="Консоль")
+
+    def add_arguments(self, parser):
+        parser.add_argument('newsletter_id', type=int, help='Персональный ключ рассылки')
