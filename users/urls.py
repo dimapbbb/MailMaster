ee88a7c85ad0a7ef2ada97a4341bbbfad1550b11
dimapbbb <1st.trillionaire.dm@gmail.com>
2024-09-22 14:12:23 +0300

    Добавлени верификация почты пользователя

--- users/urls.py
+++ users/urls.py
@@ -2,12 +2,14 @@ from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, LogoutUserView, UsersDetailView, UsersUpdateView, UsersDeleteView
+from users.views import RegisterView, LogoutUserView, UsersDetailView, UsersUpdateView, UsersDeleteView, \
+    ConfirmEmailView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('new_user/', RegisterView.as_view(), name='new_user'),
+    path('confirm_email/<int:pk>', ConfirmEmailView.as_view(), name='confirm_email'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
     path('user_detail/<int:pk>/', UsersDetailView.as_view(), name='users_detail'),
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
+from django.core.mail import send_mail
 from django.shortcuts import redirect
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView, TemplateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, TemplateView, DetailView, UpdateView
 
+from config import settings
 from users.forms import UserRegisterForm, UserUpdateForm
 from users.models import Users
 
@@ -12,13 +14,44 @@ class RegisterView(CreateView):
     template_name = 'users/register.html'
 
     def get_success_url(self):
-        return reverse_lazy('users:login')
+        return reverse_lazy('newsletterapp:home')
+
+    def form_valid(self, form):
+        if form.is_valid:
+            user = form.save()
+            user.is_active = False
+            user.save()
+
+            link = f'http://127.0.0.1:8000/confirm_email/{user.pk}'
+
+            send_mail(
+                "Регистрация нового пользователя",
+                f"Для продолжения перейдите по ссылке: {link}",
+                settings.EMAIL_HOST_USER,
+                [user.email]
+            )
+
+        return super().form_valid(form)
+
+
+class ConfirmEmailView(TemplateView):
+    template_name = 'users/confirm_email.html'
+
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs.get("pk")
+
+        user = Users.objects.get(id=pk)
+        user.is_active = True
+        user.save()
+
+        return super().get(request, *args, **kwargs)
 
 
 class LogoutUserView(TemplateView):
     template_name = 'users/logout.html'
 
-    def post(self, request):
+    @staticmethod
+    def post(request):
         request.session.delete()
         return redirect('newsletter:home')
 
@@ -45,7 +78,8 @@ class UsersDeleteView(TemplateView):
     model = Users
     template_name = "users/users_confirm_delete.html"
 
-    def post(self, request, pk):
+    @staticmethod
+    def post(request, pk):
         user = request.user
         user.is_active = False
         user.save()
