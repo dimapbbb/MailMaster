3b5d1b11d36b162902aec4057ea5c43c696c5a1d
dimapbbb <1st.trillionaire.dm@gmail.com>
2024-09-27 14:40:03 +0300

    refactoring

--- config/settings.py
+++ config/settings.py
@@ -106,7 +106,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = "genadjev.edmund@yandex.ru"
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
