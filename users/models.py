27ae801aa1f4753994e99967a15f1722fc1da862
dimapbbb <1st.trillionaire.dm@gmail.com>
2024-09-30 19:35:38 +0300

    в модель пользователя добавлены поля номер телефона и страна

--- users/models.py
+++ users/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Users(AbstractUser):
     photo = models.ImageField(upload_to="users_photo/", verbose_name="фотография", blank=True, null=True)
+    phone = models.IntegerField(verbose_name="Номер телефона", blank=True, null=True)
+    country = models.CharField(max_length=50, verbose_name="Страна", blank=True, null=True)
 
     def __str__(self):
         return f"{self.username}"
